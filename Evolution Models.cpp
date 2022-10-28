a07e0fda6ba195235be54002515bdb9f572b61c3
RT726 <98299610+RT726@users.noreply.github.com>
2022-10-28 17:21:49 -0600

    Add files via upload

---
+++ Evolution Models.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+using namespace std;
+
+class World{
+    private: Creature createCreature(){
+
+    }
+}
+
+
+class InteractiveEnvironment{
+    void update(){
+
+    }
+}
+
+class Creature {
+    private: Creature createCreature(){
+
+    }
+    private: void die(){
+
+    }
+    private: Creature reproduce(){
+
+    }
+}
+
+class Actions {
+    private:
+        Creature createCreature() {
+    
+        }
+    public:
+        float spawnChance() {
+            
+        }
+
+        float dieChance() {
+            
+        }
+
+        float reproduceChcne() {
+
+        }
+}
+
+int main()
+{
+    return 0;
+}
